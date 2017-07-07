f40c1fa42dbb693117afbc64c4209e2531bbca63
Kenta MIYACHI <kenta.miyachi@music.yamaha.com>
2017-07-07 09:14:49 +0900

    2章終わり

--- public/app.js
+++ public/app.js
@@ -1,7 +1,26 @@
 'use strict';
 
 let learnjs = {};
+
+learnjs.problemView = function(problemNumber) {
+  let title = 'Problem #' + problemNumber + ' Coming soon!';
+  return $('<div class="problem-view">').text(title);
+}
+
 learnjs.showView = function(hash) {
-  let problemView = $('<div class="problem-view">').text('Coming soon!');
-  $('.view-container').empty().append(problemView);
+  let routes = {
+    '#problem': learnjs.problemView
+  };
+  let hashParts = hash.split('-');
+  let viewFn = routes[hashParts[0]];
+  if (viewFn) {
+    $('.view-container').empty().append(viewFn(hashParts[1]));
+  }
+}
+
+learnjs.appOnReady = function() {
+  window.onhashchange = () => {
+    learnjs.showView(window.location.hash);
+  };
+  learnjs.showView(window.location.hash);
 }
